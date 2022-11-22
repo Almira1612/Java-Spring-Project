24f4202b69cf82de6d31ebf100b521a257f5ef78
Aimila Maimaitijiang <almira1612@gmail.com>
2022-11-21 22:41:54 -0500

    Added Component classes

---
+++ src/main/java/com/cydeo/stereotype_annotation/ConfigCourse.java
@@ -0,0 +1,9 @@
+package com.cydeo.stereotype_annotation;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
+public class ConfigCourse {
+}
---
+++ src/main/java/com/cydeo/stereotype_annotation/CydeoApp.java
@@ -0,0 +1,8 @@
+package com.cydeo.stereotype_annotation;
+
+public class CydeoApp {
+    public static void main(String[] args) {
+
+
+    }
+}
